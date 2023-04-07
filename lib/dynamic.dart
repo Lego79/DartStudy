17462a9e06f08045ccc868527ce34fe14c1e886a
Lego79 <kentcandoit89@gmail.com>
2023-04-07 17:15:54 +0900

    Normad Coder dart study

---
+++ lib/dynamic.dart
@@ -0,0 +1,20 @@
+void main() {
+
+  String name = 'kor';
+
+  name = '코';
+
+  dynamic people;
+
+  if(people is String) {
+
+  }
+
+  if(people is int) {
+
+    people.
+  }
+
+  //dynamic은 정말 필요할 때만 사용해야 합니다
+
+}다
---
+++ lib/test.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = '강후';
+  name = 'Kang hoo';
+  var names = 'nico';
+  //var을 쓰면 어차피 스트링 타입인 것을 알아 챈다
+  //스트링으로 지정했어도, 타입만 맞으면 name 변수에 다른 별수로 업데이트 가능하다
+  print(name + names);
+
+
+}
